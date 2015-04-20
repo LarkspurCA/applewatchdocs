6ee97156353926647e58bc9c89a4390e60c7f19d
Michael Hahn <michaelHahn@MacBook-Air.local>
2015-04-19 17:42:02 -0700

    Use RTD_NEW_THEME

--- conf.py
+++ conf.py
@@ -108,6 +108,8 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+RTD_NEW_THEME = True
+
 # html_theme_path = ["_themes", ]
 # html_theme = 'default'
 
